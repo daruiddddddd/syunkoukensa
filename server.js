6025b4ddbf62a4cdd4d79b8fbac1463232aa3cda
daruiddddddd <ryu08ma@gmail.com>
2025-05-22 16:33:34 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
